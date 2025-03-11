8ba7caabbbb4bc9ef940f90f7b714620b295e45e
socoldkiller <2119306250@qq.com>
2025-03-12 00:12:12 +0800

    refactor: modify the test command to judge command

--- cmd/judge.go
+++ cmd/judge.go
@@ -4,13 +4,14 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"github.com/spf13/cobra"
 	"os"
 	"strings"
 	"windy-judge/internal/F"
 	"windy-judge/internal/command"
 	"windy-judge/internal/parser"
 	"windy-judge/internal/report"
+
+	"github.com/spf13/cobra"
 )
 
 func selectTestCaseParser(s string) parser.TestCaseParser {
@@ -28,22 +29,23 @@ func parseCmdArgs(arg string) []string {
 	return strings.Fields(arg)
 }
 
-// testCmd represents the test command
-var testCmd = &cobra.Command{
-	Use:   "test",
+// judgeCmd represents the judge command
+var judgeCmd = &cobra.Command{
+	Use:   "judge",
 	Short: "Run a test case with the specified command and input source.",
 	Long: `The "test" command is used to execute a specified command with an input source, 
 which can be either a local file path or a URL. The command runs the test case, 
 captures the program output, compares it with the expected output, and generates a detailed report.
 
 Example usage:
-  ./windy-judge test <command> <input_source>
+  ./windy-judge judge <command> <input_source>
 
 Example:
-  ./windy-judge test cat https://example.com
+  ./windy-judge judge cat https://example.com
 
 This command will execute "cat" with the input file located at "https://example.com",
 compare the output with the expected result, and generate a report indicating whether the test passed or failed.`,
+
 	Args: cobra.ExactArgs(2),
 	Run: func(c *cobra.Command, args []string) {
 		terminal := new(F.Terminal)
@@ -65,15 +67,15 @@ compare the output with the expected result, and generate a report indicating wh
 }
 
 func init() {
-	rootCmd.AddCommand(testCmd)
+	rootCmd.AddCommand(judgeCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// testCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// judgeCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// testCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// judgeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
